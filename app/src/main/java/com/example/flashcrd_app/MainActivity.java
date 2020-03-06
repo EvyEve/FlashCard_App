4ece0af60e91339ea36b96ec28287f7eaf1a047c
EvyEve <cedent.eva@gmail.com>
2020-03-06 17:08:38 -0500

    Part of optionals added

--- app/src/main/java/com/example/flashcrd_app/MainActivity.java
+++ app/src/main/java/com/example/flashcrd_app/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,10 +13,19 @@ public class MainActivity extends AppCompatActivity {
 
         //OnClickListener is how Android handles users actions / interaction with the app
         findViewById(R.id.frontQuestion).setOnClickListener(new View.OnClickListener() {
+            //OnClickListener to show answer & hide question
             @Override
             public void onClick(View v) {
                 findViewById(R.id.frontQuestion).setVisibility(View.INVISIBLE);
                 findViewById(R.id.answerQuestion).setVisibility(View.VISIBLE);
+            }
+        });
+        //OnClicklistener to show question & hide answer
+        findViewById(R.id.answerQuestion).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answerQuestion).setVisibility(View.INVISIBLE);
+                findViewById(R.id.frontQuestion).setVisibility(View.VISIBLE);
             }
         });
     }
