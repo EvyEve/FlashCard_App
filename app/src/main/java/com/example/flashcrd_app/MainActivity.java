b96e9d7f0d73184893665afc455a5806a83dcdcc
EvyEve <cedent.eva@gmail.com>
2020-03-19 16:06:52 -0400

    Added styling of edit text. Personalized colored floating hint labels for corresponding text field

--- app/src/main/java/com/example/flashcrd_app/MainActivity.java
+++ app/src/main/java/com/example/flashcrd_app/MainActivity.java
@@ -38,30 +38,30 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.choice1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.choice1).setBackgroundColor(getResources().getColor(R.color.red));
-                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.green));
+                findViewById(R.id.choice1).setBackground(getResources().getDrawable(R.drawable.wrong_choice_background));
+                findViewById(R.id.choice3).setBackground(getResources().getDrawable(R.drawable.correct_choice_background));
             }
         });
         findViewById(R.id.choice2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.choice2).setBackgroundColor(getResources().getColor(R.color.red));
-                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.green));
+                findViewById(R.id.choice2).setBackground(getResources().getDrawable(R.drawable.wrong_choice_background));
+                findViewById(R.id.choice3).setBackground(getResources().getDrawable(R.drawable.correct_choice_background));
             }
         });
         findViewById(R.id.choice3).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.green));
+                findViewById(R.id.choice3).setBackground(getResources().getDrawable(R.drawable.correct_choice_background));
             }
         });
         //Textview to reset choices back to original look before choice was made
         findViewById(R.id.resetAnswers).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.choice1).setBackgroundColor(getResources().getColor(R.color.orange));
-                findViewById(R.id.choice2).setBackgroundColor(getResources().getColor(R.color.orange));
-                findViewById(R.id.choice3).setBackgroundColor(getResources().getColor(R.color.orange));
+                findViewById(R.id.choice1).setBackground(getResources().getDrawable(R.drawable.choices_backgroung));
+                findViewById(R.id.choice2).setBackground(getResources().getDrawable(R.drawable.choices_backgroung));
+                findViewById(R.id.choice3).setBackground(getResources().getDrawable(R.drawable.choices_backgroung));
             }
         });
         //Toggling card choice visibility via ic_open_eye icon & ic_close_eye icon
@@ -99,12 +99,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                //Grabbing the text from textview & storing it in a string
+
+                //Grabbing the text from MainActivity textview & storing it in a string
                 String questEdit = ((TextView) findViewById(R.id.frontQuestion)).getText().toString();
                 String ansEdit = ((TextView) findViewById(R.id.answerQuestion)).getText().toString();
-                //Passing the string data to the intent for the next activity
+                String wrongEdit1 = ((TextView) findViewById(R.id.choice1)).getText().toString();
+                String wrongEdit2 = ((TextView) findViewById(R.id.choice2)).getText().toString();
+
+                //Passing the string data from MainActivity to the intent for the next activity
                 intent.putExtra("qEdit", questEdit);
                 intent.putExtra("aEdit",ansEdit);
+                intent.putExtra("wEdit1", wrongEdit1);
+                intent.putExtra("wEdit2", wrongEdit2);
+
                 //Start Next Activity & Specify Data Is Expected To Be Returned
                 MainActivity.this.startActivityForResult(intent, 100);
             }
@@ -118,9 +125,13 @@ public class MainActivity extends AppCompatActivity {
             //Storing String values from previous activity
             String question = data.getExtras().getString("questString");
             String answer = data.getExtras().getString("anString");
+            String wrgChoice1 = data.getExtras().getString("wrongCh1");
+            String wrgChoice2 = data.getExtras().getString("wrongCh2");
 
             ((TextView) findViewById(R.id.frontQuestion)).setText(question);
             ((TextView) findViewById(R.id.answerQuestion)).setText(answer);
+            ((TextView) findViewById(R.id.choice1)).setText(wrgChoice1);
+            ((TextView) findViewById(R.id.choice2)).setText(wrgChoice2);
             ((TextView) findViewById(R.id.choice3)).setText(answer);
         }
         //Snackbar display message once card has been successfully created
